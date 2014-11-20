f7fde270f44009d3e95d705a08e8ada21dea5edd
pawel <pawel.niechoda@gmail.com>
2014-11-20 22:29:56 +0200

    add initial layout support

--- app/src/main.js
+++ app/src/main.js
@@ -2,30 +2,45 @@
 define(function(require, exports, module) {
 	'use strict';
 
-	var _ = require('underscore');
 	var Engine = require('famous/core/Engine');
+	var HeaderFooterLayout = require('famous/views/HeaderFooterLayout');
+	var StateModifier = require('famous/modifiers/StateModifier');
 	var Gameplay = require('Gameplay');
 	var PointCounter = require('PointCounter');
 	var PointsBoard = require('PointsBoard');
 	var Modal = require('Modal');
+	var ContainerSurface = require('famous/surfaces/ContainerSurface');
+
+	var containerSurface = new ContainerSurface({
+		size: [300, 530]
+	});
+	var layout = new HeaderFooterLayout({
+		footerSize: 30
+	});
+	var modifier = new StateModifier({
+		align: [0.5, 0],
+		origin: [0.5, 0]
+	});
+	containerSurface.add(layout);
 
 	var mainContext = Engine.createContext();
 	var modal = new Modal(mainContext, 'Game Over');
 	var pointCounter = new PointCounter();
-	var pointsBoard = new PointsBoard(mainContext, pointCounter);
-
+	var pointsBoard = new PointsBoard(layout.footer, pointCounter);
 
 	modal.setOnClickCallback(function(){
 	    gameplay.restart();
 		modal.hide();
 	});
 
-
-	var gameplay = new Gameplay(mainContext);
+	var gameplay = new Gameplay(layout.content);
 	gameplay.setPointCounter(pointCounter);
 	gameplay.setOnGameOverCallback(function(){
 		modal.show();
 	});
 	gameplay.start();
+
+
+	mainContext.add(modifier).add(containerSurface);
 
 });
